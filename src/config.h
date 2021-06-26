a70dbfc9dc54bafa84c6cec27be0cea5f5345a8c
Ramon Melo <ramonmelo@ramonmelo.com>
2021-06-26 18:43:31 -0300

    CHANGED: Log Messages

--- src/config.h
+++ src/config.h
@@ -25,8 +25,9 @@ const char *MQTT_DATA_TOPIC("v1/devices/me/telemetry");
 const String STR_FAIL(" fl");        // fail
 const String STR_SUCCESS(" sc");     // success
 const String STR_CONNECTING("ctg "); // connecting
-const String STR_INIT("it...");      // init
-const String STR_NETWORK("nt...");   // network
+const String STR_INIT("it.");        // init
+const String STR_NETWORK("nt.");     // network
+const String STR_RESET("rt.");       // reset
 
 // Codes:
 const String CODE_M1("M1"); // MQTT NOT CONNECTED
