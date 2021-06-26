5163f1e0ae97a581434934eef2bbe459b362f8eb
Ramon Melo <ramonmelo@ramonmelo.com>
2021-06-26 15:05:43 -0300

    REMOVED: Old Code

--- lib/Sensors/SensorTurbidity.h
+++ lib/Sensors/SensorTurbidity.h
@@ -28,30 +28,6 @@ namespace smartaqua
 
     void SensorTurbidity::service()
     {
-        // float NTU = 0;
-        // float reading = 0;
-
-        // for (size_t i = 0; i < N_AVERAGE; i++)
-        // {
-        //     reading += ((float)analogRead(reading) / (1023)) * 5;
-        // }
-
-        // reading = reading / N_AVERAGE;
-        // reading = roundf(reading);
-
-        // if (reading < 2.5)
-        // {
-        //     NTU = 3000;
-        // }
-        // else if (reading > 4.2)
-        // {
-        //     NTU = 0;
-        // }
-        // else
-        // {
-        //     NTU = (-1120.4 * reading * reading) + (5742.3 * reading) - 4353.8;
-        // }
-
         setData(0, readAsAnalog(analogPin));
     }
 
